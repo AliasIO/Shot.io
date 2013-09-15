39d9eddcc0229ba6f7a80699cc365f920e56e90d
Elbert Alias <elbert@alias.io>
2013-09-15 20:08:49 +1000

    Replace smart thumbnail generator with custom implementation

--- Shot/Controllers/Upload.php
+++ Shot/Controllers/Upload.php
@@ -42,6 +42,12 @@ class Upload extends \Swiftlet\Controller
 		);
 
 	/**
+	 * Thumbnail size
+	 * @var integer
+	 */
+	static $thumbnailSize = 480;
+
+	/**
 	 * Default action
 	 */
 	public function index()
@@ -103,6 +109,10 @@ class Upload extends \Swiftlet\Controller
 		}
 	}
 
+	/**
+	 * Generate various file sizes
+	 * @param string $filename
+	 */
 	protected function exportSizes($filename)
 	{
 		foreach ( self::$imageSizes as $imageSize ) {
@@ -119,11 +129,113 @@ class Upload extends \Swiftlet\Controller
 			$image->writeImage(self::$uploadPath . $imageSize . '/' . $filename);
 		}
 
-		// Thumbnail
-		$entropy = new \stojg\crop\CropEntropy(self::$uploadPath . $filename);
+		$this->exportThumbnail($filename);
+	}
 
-		$thumbnail = $entropy->resizeAndCrop(480, 480);
+	/**
+	 * Generate thumbnail
+	 * @param string $filename
+	 */
+	protected function exportThumbnail($filename)
+	{
+		$thumbnail = new \Imagick(self::$uploadPath . $filename);
 
-		$thumbnail->writeimage(self::$uploadPath . '480/' . $filename);
+		$geometry = $thumbnail->getImageGeometry();
+
+		if ( $geometry['width'] >= $geometry['height'] ) {
+			$orientation = 'x';
+
+			$thumbnail->scaleImage(0, self::$thumbnailSize);
+		} else {
+			$orientation = 'y';
+
+			$thumbnail->scaleImage(self::$thumbnailSize, 0);
+		}
+
+		$geometry = $thumbnail->getImageGeometry();
+
+		$size = array(
+			'x' => $geometry['width'],
+			'y' => $geometry['height']
+			);
+
+		// Prepare image to improve entropy calculation
+		$image = clone($thumbnail);
+
+		// Greyscale
+		$image->modulateImage(100, 0, 100);
+
+		// Find edges
+		//$image->edgeimage(5);
+
+		// Blur
+		$image->blurImage(3, 2);
+
+		// Slice image
+		$sliceCount = 25;
+
+		$sliceSize = floor($size[$orientation] / $sliceCount);
+
+		$entropy = array();
+
+		// Obtain entropy value for each slice
+		for ( $i = 0; $i < $sliceCount; $i ++ ) {
+			$slice = clone($image);
+
+			if ( $orientation == 'x' ) {
+				$slice->cropImage($sliceSize, $size['x'], $sliceSize * $i, 0);
+			} else {
+				$slice->cropImage($size['y'], $sliceSize, 0, $sliceSize * $i);
+			}
+
+			$slice->writeimage(self::$uploadPath . 'slice/' . $orientation . $i . '.jpg');
+
+			$entropy[$i] = $this->getEntropy($slice, $size[$orientation] * $sliceSize) . "\n";
+		}
+
+		$thumbnailSliceCount = floor(self::$thumbnailSize / $sliceSize);
+
+		$entropySums = array();
+
+		// For each possible offset, calculate the total entropy value
+		for ( $i = 0; $i < $sliceCount - $thumbnailSliceCount; $i ++ ) {
+			$entropySums[$i] = 0;
+
+			for ( $j = 0; $j < $thumbnailSliceCount; $j ++ ) {
+				$entropySums[$i] += $entropy[$i + $j];
+			}
+		}
+
+		// Choose the offset with the most available entropy
+		$offset = array_search(max($entropySums), $entropySums);
+
+		if ( $orientation == 'x' ) {
+			$thumbnail->cropImage(self::$thumbnailSize, self::$thumbnailSize, $offset * $sliceSize, 0);
+		} else {
+			$thumbnail->cropImage(self::$thumbnailSize, self::$thumbnailSize, 0, $offset * $sliceSize);
+		}
+
+		$thumbnail->writeimage(self::$uploadPath . self::$thumbnailSize . '/' . $filename);
+	}
+
+	/**
+	 * Calculate entropy
+	 * @param \ImageMagick $image
+	 * @param integer $area
+	 */
+	protected function getEntropy(\Imagick $image, $area)
+	{
+		$histogram = $image->getImageHistogram();
+
+		$value = .0;
+
+		for ( $i = 0; $i < count($histogram); $i ++ ) {
+			// Percentage of pixels having this color value
+			$pixels = $histogram[$i]->getColorCount() / $area;
+
+			$value -= $pixels * log($pixels, 2);
+		}
+
+		return $value;
 	}
 }
